7059928240e89d1fadcd0b70009801fcd2bda4dd
Martijn <martijncvv@gmail.com>
2021-06-15 23:13:11 +0200

    Added setup of  pages and navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+	return (
+		<div>
+			<Link to="/" exact>
+				Home
+			</Link>
+			{" - "}
+			<Link to="/dashBoard">Dashboard</Link>
+			{" - "}
+			<Link to="/product">Product</Link>
+			{" - "}
+			<Link to="/">Register</Link>
+			{" - "}
+			<Link to="/">Login</Link>
+		</div>
+	);
+}
